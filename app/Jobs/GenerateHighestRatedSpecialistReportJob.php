6220db283461d76472af5adb767ba7fc114344df
Bruno <brunozorzetto@gmail.com>
2023-10-05 00:42:50 -0300

    feat: add new mail to send to user with specialist report link to download

--- app/Jobs/GenerateHighestRatedSpecialistReportJob.php
+++ app/Jobs/GenerateHighestRatedSpecialistReportJob.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Mail\SpecialistReport;
 use App\Models\Specialist;
 use Exception;
 use Illuminate\Bus\Queueable;
@@ -12,6 +13,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 class GenerateHighestRatedSpecialistReportJob implements ShouldQueue
@@ -41,13 +43,21 @@ class GenerateHighestRatedSpecialistReportJob implements ShouldQueue
             ->orderBy('avg_rating', 'desc')
             ->limit(10)
             ->get();
-    
+
+            
+            /**
+             * @TODO - the best to do this is saving in a external storage, such as S3, Google Cloud Storage, etc.
+             * It's just for a simple implementation to save a file in somewhere.
+            */
             $fileName = 'highest_rated_specialists_' . time() . '.csv';
             $disk = Storage::disk('public');
+
             foreach ($specialists as $specialist) {
                 $content = "{$specialist->id}, {$specialist->name}, {$specialist->avg_rating}";
                 $disk->append($fileName, $content);
             }
+
+            Mail::to($this->userEmail)->send(new SpecialistReport($disk->url($fileName)));
         } catch (Exception $ex) {
             Log::error($ex->getMessage());
         } finally {
